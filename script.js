f3aafce93aa4592322151ddedcb0c978d308c0b0
Mavlon <mavlon.idc@mail.ru>
2020-12-03 09:04:33 +0500

    js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let stepen = +prompt('Введите степень')
 let result = 1
 
 number = !isNaN(number) && !number == 0 ? number : number;
-stepen = !isNaN(stepen) || !stepen == 0 ? stepen : 2 ;
+stepen = NaN || !stepen == 0 ? stepen : 2 ;
 
 for(let counter=0; counter < stepen; counter++){
     result = result * number
